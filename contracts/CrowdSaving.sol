728600cb44b0b020140ef958fb9a6fc93a58cad1
tshepo_mokoena <tshepomokoena12@gmail.com>
2020-07-28 00:07:28 +0200

    improved how strart project works

--- contracts/CrowdSaving.sol
+++ contracts/CrowdSaving.sol
@@ -3,15 +3,15 @@
 pragma solidity >=0.5.4 <0.7.0;
 // Importing OpenZeppelin's SafeMath Implementation
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
-import '../contracts/Project.sol';
-
+import "./Project.sol";
 /** @title CrowdSaving */
 contract CrowdSaving {
     using SafeMath for uint256;
 
     // List of existing projects (Contract Objects)
-    Project[] private projects;
+    uint public numOfProjects;
 
+    mapping (uint => address) public projects;
     // Event that will be emitted whenever a new project is started
     event ProjectStarted(
         address contractAddress,
@@ -30,34 +30,29 @@ contract CrowdSaving {
       * @param durationInDays Project deadline in days
       * @param amountToRaise Project goal in wei
       */
-    function startProject(
+       function startProject(
         string calldata title,
         string calldata description,
         int  numberContributors,
         uint durationInDays,
         uint amountToRaise
-        ) external {
-        uint raiseUntil = now.add(durationInDays.mul(1 days));
-        Project newProject = new Project(msg.sender, title, description,numberContributors, raiseUntil, amountToRaise);
-        projects.push(newProject);
-        
-        emit ProjectStarted(
-            address(newProject),
-            msg.sender,
-            title,
-            description,
-            newProject.distributedAmount(),
-            numberContributors,
-            raiseUntil,
-            amountToRaise
-        );
-    }                                                                                                                                   
-
-    /** @dev Function to get all projects' contract addresses.
-      * @return A list of all projects' contract addreses
-      */
-    function returnAllProjects() external view returns(Project[] memory){
-        return projects;
+        ) external  payable returns (address projectAddress)  {
+            // solium-disable-next-line security/no-block-members
+            uint raiseUntil = now.add(durationInDays.mul(1 days));
+            Project newProject = new Project(msg.sender, title, description,numberContributors, raiseUntil, amountToRaise);
+            projects[numOfProjects] = newProject;
+            numOfProjects++;
+            emit ProjectStarted(
+                address(newProject),
+                msg.sender,
+                title,
+                description,
+                newProject.distributedAmount(),
+                numberContributors,
+                raiseUntil,
+                amountToRaise
+                );
+            return address(newProject);
     }
 }
 
